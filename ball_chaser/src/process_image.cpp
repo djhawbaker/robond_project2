a5d6ad833ff7a51fe9e8a62437bc6753d05cf99d
djhawbaker <djhawbaker@gmail.com>
2019-02-27 08:53:01 -0800

    Wrote the logic for process image and am publishing the drive commands

--- ball_chaser/src/process_image.cpp
+++ ball_chaser/src/process_image.cpp
@@ -9,6 +9,16 @@ ros::ServiceClient client;
 void drive_robot(float lin_x, float ang_z)
 {
     // TODO: Request a service and pass the velocities to it to drive the robot
+
+    ball_chaser::DriveToTarget srv;
+
+    srv.request.linear_x = lin_x;
+    srv.request.angular_z = ang_z;
+
+    if ( !client.call(srv) )
+    {
+        ROS_ERROR( "Failed to call service ball_chaser" );
+    }
 }
 
 // This callback function continuously executes and reads the image data
@@ -16,15 +26,57 @@ void process_image_callback(const sensor_msgs::Image img)
 {
 
     int white_pixel = 255;
-    int i;
+    int i, j;
+    // Divide image into 3 equal width sections. These are the left and right dividing lines
+    int left = img.width / 3;
+    int right = left * 2;
+    bool ball_found = false;
+    float forward_speed = 5;
+    float stop = 0.0;
+    // Turn 5 degrees / second in radians
+    float turn_left = -0.0872665;
+    float turn_right = 0.0872665;
+
 
     // TODO: Loop through each pixel in the image and check if there's a bright white one
     // Then, identify if this pixel falls in the left, mid, or right side of the image
     // Depending on the white ball position, call the drive_bot function and pass velocities to it
     // Request a stop when there's no white ball seen by the camera
-    for ( i = 0; i < img.width(); ++i)
+    for ( i = 0; i < img.height(); ++i)
     {
-        // TODO
+        for ( j = 0; j < img.width(); ++j)
+        {
+            if ( white_pixel == img.data[i][j] )
+            {
+                ball_found = true;
+                if ( img.data[i][j] < left )
+                {
+                    // Ball is to the left, turn left
+                    drive_robot( forward_speed, turn_left );
+                }
+                else if (img.data[i][j] > right )
+                {
+                    // Ball is to the right, turn right 
+                    drive_robot( forward_speed, turn_right );
+                }
+                else
+                {
+                    // Ball is straight ahead, chase it!
+                    drive_robot( forward_speed, stop );
+                }
+                break;
+            }
+        }
+        if ( ball_found == true )
+        {
+            break;
+        }
+    }
+
+    if ( ball_found == false )
+    {
+        // Stop the bot
+        drive_robot( stop, stop );
     }
 }
 
